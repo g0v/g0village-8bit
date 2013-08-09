f7eebe1ceb24b58171519890af8670de6a2f141c
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-08-09 21:54:00 +0800

    add new npc 鄉民健忘症主治醫師.

--- scenes/overworld.js
+++ scenes/overworld.js
@@ -165,6 +165,108 @@ window.overworldScene = function () {
             enterCallback: enter
         }
     })()
+    hychenScript = (function () {
+        var counter = 0;
+        var choice1 = 0;
+        var space = function () {
+            if (!vnEngine.isWriting() && !vnEngine.isAnimating()) {
+                switch (counter) {
+                    case 0:
+                        vnEngine.hideInteraction();
+
+                        vnEngine.setName("鄉民健忘症主治醫師");
+                        vnEngine.setText(Hero.name + " 你好!");
+                        vnEngine.setPortrait("assets/yosuke.png");
+                        vnEngine.showDialog();
+                        break;
+                    case 1:
+                        var _hychen_bala = ["做壞事,神明不一定馬上處罰你,但說錯話,鄉民馬上就來桶你了...",
+                                               "這是個『舉頭三尺有鄉民』的時代。",
+                                               "鄉民關心你，專治鄉民健忘症",
+                                               "我本來是個型男，但自從我膝蓋中了一鍵...",
+                                               "我要是有點嘴泡專精，就不會是這種鍵樣",
+                                               "你見過高村長了嗎? 他在右邊喔!"];
+		        vnEngine.setText(_hychen_bala[_.random(0, _hychen_bala.length)]);
+                        vnEngine.animateMessage();
+                        break;
+		    case 2:
+		        vnEngine.setText(Hero.name + " 你想加入鄉民關心你這個專案嗎?");
+                            $.when(vnEngine.animateMessage()).then(function () {
+                                $.when(vnEngine.promptQuestion(["算我一個!", "這是什麼?", "不想耶!"])).then(function (choice) {
+                                     choice1 = choice;
+                                     switch (choice1) {
+                                            case 1:                           
+                                                vnEngine.setText("非常好！");
+                                                vnEngine.animateMessage();                                                
+                                                break;
+                                            case 2:
+                                                vnEngine.setText("治療鄉民健忘症的計畫...");
+                                                vnEngine.animateMessage();
+                                                break;
+                                            case 3:
+                                                vnEngine.setText("砂鍋大的拳頭，你有....");
+                                                vnEngine.animateMessage();
+					        counter = -2;
+                                                break;
+                                        }
+                                })});                      
+                        break;
+                    case 3:
+		        switch (choice1) {
+			    case 1:
+			        vnEngine.setText("研究一下怎麼報到吧!");
+                                vnEngine.animateMessage();
+			        break;
+                            case 2:
+			        vnEngine.setText("看一下影片吧....");
+                                vnEngine.animateMessage();
+			        break;
+			}
+                        break;
+                    case 4:
+		        switch (choice1) {
+			    case 1:
+                                setTimeout(openUrlInBox('https://g0v.hackpad.com/--1OaXIxVVPSd'), 1500);
+                                vnEngine.setText("有問題的話就在IRC上問值日生吧!");
+                                vnEngine.animateMessage();
+                                counter = - 4; 
+                                break;
+			    case 2:
+                                setTimeout(openUrlInBox('http://www.youtube.com/embed/SpovzhVCg48?feature=player_detailpage'), 1500);
+                                vnEngine.setText("所以...");
+                                vnEngine.animateMessage();
+                                counter = counter - 3; 
+                                break;
+			}
+                        break;
+                    default:
+                        counter = -1;
+                        vnEngine.hideDialog();
+                        vnEngine.showInteraction();
+                }
+                counter++;
+            } else if (vnEngine.isWriting()) {
+                console.log("is writing");
+                vnEngine.forceTextFinish();
+            }
+            console.log(counter);
+        }
+        var leave = function () {
+            vnEngine.hideDialog();
+
+            vnEngine.hideInteraction();
+            counter = 0;
+        }
+        var enter = function () {
+            vnEngine.showInteraction();
+        }
+        return {
+            spacebarCallback: space,
+            leaveCallback: leave,
+            enterCallback: enter
+        }
+    })()
+
     clkaoScript = (function () {
         var counter = 0;
         var choice1 = 0;
@@ -286,6 +388,9 @@ window.overworldScene = function () {
     Crafty.sprite(32, "assets/shadowsprite.png", {
         shadowSprite: [0, 0]
     });
+    Crafty.sprite(32, "assets/yosukesprite.png", {
+        hychenSprite: [1, 0]
+    });
 
     var chie = Crafty.e("2D, Canvas, chieSprite, NPC").attr({
         x: 510,
@@ -305,6 +410,13 @@ window.overworldScene = function () {
         y: 413,
     })
         .setupScript(hlbScript)
+        .wander();
+
+    var hychen = Crafty.e("2D, Canvas, hychenSprite, NPC").attr({
+        x: 250,
+        y: 253,
+    })
+        .setupScript(hychenScript)
         .wander();
 
     var player1 = Crafty.e("Player").attr({
