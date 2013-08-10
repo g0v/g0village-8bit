dc803dc062ce39774bbeb6fa19213fd9b2640fc3
Rack Lin <racklin@gmail.com>
2013-08-11 00:14:17 +0800

    Added g0v news to racklin Signed-off-by: Rack Lin <racklin@gmail.com>

--- scenes/overworld.js
+++ scenes/overworld.js
@@ -84,27 +84,65 @@ window.overworldScene = function () {
                         vnEngine.showDialog();
                         counter = 1;
                         break;
+
                     case 1:
-                        vnEngine.setText(Hero.name + "! 你知道我是誰嗎?");
+                        vnEngine.setText("g0v 最近很多活動唷，想聽聽大家怎麼說？");
+                        $.when(vnEngine.animateMessage()).then(function () {
+                            $.when(vnEngine.promptQuestion(["魔術", "第一次Hackth4n(新手文)","做事不用會寫程式"])).then(function (choice) {
+                                switch (choice) {
+                                    case 1:
+                                        Crafty.audio.muteMusic('music');
+                                        openUrlInBox('http://blog.g0v.tw/post/57884845324', {
+                                            onClosed: function() {
+                                                Crafty.audio.unmuteMusic('music');
+                                            }
+                                        });
+                                        break;
+                                    case 2:
+                                        Crafty.audio.muteMusic('music');
+                                        openUrlInBox('http://blog.g0v.tw/post/57882027353', {
+                                            onClosed: function() {
+                                                Crafty.audio.unmuteMusic('music');
+                                            }
+                                        });
+                                        break;
+                                    case 3:
+                                        Crafty.audio.muteMusic('music');
+                                        openUrlInBox('http://blog.g0v.tw/post/57878341290', {
+                                            onClosed: function() {
+                                                Crafty.audio.unmuteMusic('music');
+                                            }
+                                        });
+                                        break;
+                                }
+                                vnEngine.setText("燃起你的熱情了嗎？");
+                                vnEngine.animateMessage();
+                                counter = 2;
+                            });
+                        });
+                        break;
+
+                    case 2:
+                        vnEngine.setText(Hero.name + "! 對了，你知道我是誰嗎?");
                         $.when(vnEngine.animateMessage()).then(function () {
                             $.when(vnEngine.promptQuestion(["知道!", "不知道!"])).then(function (choice) {
                                 switch (choice) {
                                     case 1:
                                         vnEngine.setPortrait("assets/racklin.png");
                                         vnEngine.setText("噓! 我現在身份是新手村的掃地僧!");
-                                        counter = 2;
+                                        counter = 3;
                                         break;
                                     case 2:
                                         vnEngine.setPortrait("assets/racklin.png");
                                         vnEngine.setText("年輕的時侯來到這島建設，回想當時我也很『萌』.");
-                                        counter = 2;
+                                        counter = 3;
                                         break;
                                 }
                                 vnEngine.animateMessage();
                             });
                         })
                         break;
-                    case 2:
+                    case 3:
                         vnEngine.setText("你去找左下角的萌典小精靈學習吧!");
                         $.when(vnEngine.animateMessage()).then(function () {
                             //derp
@@ -258,7 +296,12 @@ window.overworldScene = function () {
                         vnEngine.setText("你想要利用「萌典」學習什麼成語嗎？");
                         $.when(vnEngine.animateMessage()).then(function () {
                             $.when(vnEngine.promptQuestion(w)).then(function (choice) {
-                                openUrlInBox('https://moedict.tw/#' + w[choice - 1]);
+                                Crafty.audio.muteMusic('music');
+                                openUrlInBox('https://moedict.tw/#' + w[choice - 1], {
+                                    onClosed: function() {
+                                        Crafty.audio.unmuteMusic('music');
+                                    }
+                                });
                                 vnEngine.setText("你現在知道 " + w[choice - 1] + " 的意思了！ LV+1");
                                 Hero.followers++;
                                 Hero.contributions += 10;
