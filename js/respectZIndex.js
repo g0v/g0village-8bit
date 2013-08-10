a48cb34f8a4f385e54ce6581fbc700dd33c30238
Audrey Tang <audreyt@audreyt.org>
2013-08-11 01:13:17 +0800

    * Debounce zindex engine to address #7.

--- js/respectZIndex.js
+++ js/respectZIndex.js
@@ -1,15 +1,28 @@
 var __guidIndex = 0;
+var __cached_chrs;
+var __cached_time;
 Crafty.c("RespectZIndex", {
     init: function () {
         this.requires("2D");
 		this.z = __guidIndex;
 		__guidIndex += 1;
         this.bind("FinishFrame", function () {
+            /* cap per-element zIndex refresh rate at 4FPS */
+            if (this.__cached_debounce > ((new Date).getTime() - 250)) { return; }
+            this.__cached_debounce = (new Date).getTime();
             // calculate z index
             // does this by swapping z index with anything below or above.
             // there might be syncing issues, but fuck that too lazy to fix
-            var chrs = new Crafty("RespectZIndex"), lcv, el, temp;
-			chrs = chrs.toArray();
+            var chrs;
+            if (__cached_chrs && (__cached_time > ((new Date).getTime() - 5000))) {
+                chrs = __cached_chrs;
+            }
+            else {
+                chrs = __cached_chrs = (new Crafty("RespectZIndex")).toArray();
+                __cached_time = (new Date).getTime();
+            }
+
+            var lcv, el, temp;
             for (lcv = 0; lcv < chrs.length; lcv += 1) {
                 el = new Crafty(chrs[lcv]);
                 // computer the center
