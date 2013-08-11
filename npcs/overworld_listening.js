046a833577c54c4c9c5b5835155a779c80f6912d
Audrey Tang <audreyt@audreyt.org>
2013-08-11 22:21:17 +0800

    * LV+1 on recognizing listening

--- npcs/overworld_listening.js
+++ npcs/overworld_listening.js
@@ -10,29 +10,36 @@ window.overworldListeningScript = function (vnEngine) {
                     vnEngine.hideInteraction();
 
                     vnEngine.setName("福利請聽");
-                    vnEngine.setText("嗨! 您好～\n一個成功的社會並不是看富裕的人過得多優渥\n而是弱勢的如何自在的生存");
+                    vnEngine.setText("嗨! 您好～\n一個成功的社會，並不是看富裕的人過得多優渥，\n而是弱勢者如何自在的生存。");
                     vnEngine.setPortrait("assets/listening.png");
                     vnEngine.showDialog();
                     counter = 1;
                     break;
 
                 case 1:
-                    vnEngine.setText("想知道 福利請聽 都在做些什麼嗎？");
+                    vnEngine.setText("想知道「福利請聽」都在做些什麼嗎？");
                     $.when(vnEngine.animateMessage()).then(function () {
                         $.when(vnEngine.promptQuestion(["想！", "不想耶！"])).then(function (choice) {
                             switch (choice) {
                                 case 1:
                                     Crafty.audio.muteMusic('music');
-                                    openUrlInBox('http://listening.g0v.tw/#/index', {
+                                    openUrlInBox('http://listening.g0v.tw/', {
                                         onClosed: function() {
                                             Crafty.audio.unmuteMusic('music');
                                         }
                                     });
-                                    vnEngine.setText("...");
+                                    if (Hero.interacted_with_listening) {
+                                        break;
+                                    }
+
+                                    Hero.followers++;
+                                    Hero.contributions += 10;
+                                    Hero.interacted_with_listening = true;
+                                    vnEngine.setText("你現在認識「福利請聽」了！ LV+1");
                                     vnEngine.animateMessage();
                                     break;
                                 case 2:
-                                    vnEngine.setText("...");
+                                    vnEngine.setText("沒關係，想知道時可以隨時問我！");
                                     counter = 3;
                                     vnEngine.animateMessage();
                                     break;
